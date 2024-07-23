51b406866a9413ed9396a7db6b1c5fb0267984c8
Slim Shady <145922247+shady21-21@users.noreply.github.com>
2024-07-23 10:37:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -27,16 +27,41 @@ import java.util.*;
 class Solution {
     // Function to find maximum product subarray
     long maxProduct(int[] arr, int n) {
+       
+       long maxPro = Long.MIN_VALUE;
+       long prod = 1;
+       for(int i = 0;i<n;i++){
+           prod *= arr[i];
+           maxPro = Math.max(maxPro, prod);
+           if(prod == 0){
+               prod = 1;
+           }
+       }
+       
+         prod = 1;
+        for(int i = n-1;i>=0;i--){
+           prod *= arr[i];
+           maxPro = Math.max(maxPro, prod);
+           if(prod == 0){
+               prod = 1;
+           }
+       }
         
-        long maxPro = Long.MIN_VALUE;
-        
-        for(int i = 0;i<n;i++){
-            long pro = 1;
-            for(int j = i;j<n;j++){
-                pro *= arr[j];
-                maxPro = Math.max(maxPro, pro);
-            }
-        }
         return maxPro;
+        
+      
+        
+        //Brute Froce Approach..
+        
+        // long maxPro = Long.MIN_VALUE;
+        
+        // for(int i = 0;i<n;i++){
+        //     long pro = 1;
+        //     for(int j = i;j<n;j++){
+        //         pro *= arr[j];
+        //         maxPro = Math.max(maxPro, pro);
+        //     }
+        // }
+        // return maxPro;
     }
 }
