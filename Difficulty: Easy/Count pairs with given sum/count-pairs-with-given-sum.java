f799bee0412313cd01f2af10025b0c63a0b2a6fc
Slim Shady <145922247+shady21-21@users.noreply.github.com>
2024-12-27 11:05:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -1,17 +1,38 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
+
+// } Driver Code Ends
+class Solution {
+    
+
+    int countPairs(int arr[], int target) {
+        // Your code here
+        
+       int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+         for(int i = 0;i<n;i++){
+            int xx = target - arr[i];
+            if(map.containsKey(xx)){
+                cnt+= map.get(xx);
+            }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        return cnt;
+    }
+}
+
+//{ Driver Code Starts.
+
 class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
+        int t = Integer.parseInt(read.readLine());
 
         while (t-- > 0) {
-            int k = Integer.parseInt(read.readLine().trim());
             String line = read.readLine().trim();
             String[] numsStr = line.split(" ");
             int[] nums = new int[numsStr.length];
@@ -19,44 +40,14 @@ class Main {
                 nums[i] = Integer.parseInt(numsStr[i]);
             }
 
+            int target = Integer.parseInt(read.readLine());
+
             Solution obj = new Solution();
-            int ans = obj.getPairsCount(nums, k);
-            System.out.println(ans);
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
         }
     }
 }
-
 // } Driver Code Ends
-
-
-// User function Template for Java
-
-class Solution {
-    int getPairsCount(int[] arr, int sum) {
-        
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        // for(int ele : arr){
-        //     map.put(ele, map.getOrDefault(ele, 0)+1);
-        // }
-        int cnt = 0;
-        for(int i = 0;i<arr.length;i++){
-            if(map.containsKey(sum-arr[i])){
-                cnt+= map.get(sum-arr[i]);
-            }
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
-        }
-        return cnt;
-        
-        //brute froce . TC -> 0(N^2)
-        /*int cnt = 0;
-        for(int i = 0;i<arr.length;i++){
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[i] + arr[j] == sum){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;*/
-    }
-}
