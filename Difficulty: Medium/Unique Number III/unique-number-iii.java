6e95a144f7da6c90a3d43ba04fb137016bb34e50
Slim Shady <145922247+shady21-21@users.noreply.github.com>
2025-04-24 10:10:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number III/unique-number-iii.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSingle(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSingle(int[] arr) {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int xx :arr)map.put(xx, map.getOrDefault(xx, 0) + 1);
+        
+        for(Map.Entry<Integer, Integer> E : map.entrySet()){
+            
+            int V = E.getValue();
+            if(V == 1){
+                return E.getKey();
+            }
+        }
+        return -1;
+    }
+}
